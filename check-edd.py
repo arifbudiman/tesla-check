3bdfedb47bb7b93ccb185642ede4ebb160cb3984
Arif Budiman <arifbudiman@gmail.com>
2021-07-27 18:25:00 -0700

    Parameterize order reference number

--- check-edd.py
+++ check-edd.py
@@ -7,12 +7,13 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 myEmail = os.getenv("EMAIL")
 myCredential = os.getenv("CREDENTIAL")
+referenceNumber = os.getenv("REFERENCE_NUMBER")
 
 userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
 
 urlLoginForm = "https://auth.tesla.com/oauth2/v1/authorize?redirect_uri=https://www.tesla.com/teslaaccount/owner-xp/auth/callback&response_type=code&client_id=ownership&scope=openid%20email&audience=https%3A%2F%2Fownership.tesla.com%2F"
 
-urlManage = "https://www.tesla.com/teslaaccount/profile?rn=RN115201757"
+urlManage = "https://www.tesla.com/teslaaccount/profile?rn=" + referenceNumber
 
 headers = {"User-Agent": userAgent}
 
@@ -40,7 +41,6 @@ with Session() as s:
     loginResponse = s.post(urlLoginForm, login_data, headers={"User-Agent": userAgent, "Referer": urlLoginForm})
 
     homepage = s.get(urlManage)
-
     homepage_content = bs(homepage.content, "html.parser")
     
     pattern = re.compile(r'\"EstimateDelivery\":{([^}]+)}')

fc2169aa4503d0d78be2e53383dda3dd1cf8e17d
Arif Budiman <arifbudiman@gmail.com>
2021-07-29 14:03:48 -0700

    Parameterized some vars and improved regex

--- check-edd.py
+++ check-edd.py
@@ -12,13 +12,10 @@ load_dotenv(verbose=True)
 myEmail = os.getenv("EMAIL")
 myCredential = os.getenv("CREDENTIAL")
 referenceNumber = os.getenv("REFERENCE_NUMBER")
-sendMail = False
-
-userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
-
-urlLoginForm = "https://auth.tesla.com/oauth2/v1/authorize?redirect_uri=https://www.tesla.com/teslaaccount/owner-xp/auth/callback&response_type=code&client_id=ownership&scope=openid%20email&audience=https%3A%2F%2Fownership.tesla.com%2F"
-
-urlManage = "https://www.tesla.com/teslaaccount/profile?rn=" + referenceNumber
+userAgent = os.getenv("USER_AGENT")
+urlLoginForm = os.getenv("LOGIN_FORM_URL")
+urlManage = os.getenv("MANAGE_URL") + referenceNumber
+sendMail = os.getenv("EMAIL_RESULT")
 
 headers = {"User-Agent": userAgent}
 
@@ -48,13 +45,13 @@ with Session() as s:
     homepage = s.get(urlManage)
     homepage_content = bs(homepage.content, "html.parser")
 
-    pattern = re.compile(r'\"EstimateDelivery\":{([^}]+)}')
+    pattern = re.compile(r'\"EstimateDelivery\"\s*:\s*{\s*\"copyOverride\"\s*:\s*\"([^\"]+)\"\s*}')
     edd = homepage_content.find("script", text=pattern)
 
     if edd:
         match = pattern.search(edd.string)
         if match:
-            if sendMail == False:
+            if sendMail == "False":
                 print(match.group(1))
             else:
                 msg = MIMEText(match.group(1))
